ff4f97074157f4cc8850d78a7e1b80c97f962fa3
KDEV999 <cristhiangrojas@gmail.com>
2026-03-03 15:41:37 -0500

    Enhance NestJS application with MongoDB and configuration management. Added @nestjs/config and @nestjs/mongoose dependencies, configured MongoDB connection using environment variables, and updated package.json and yarn.lock accordingly. Improved main.ts to support dynamic port configuration.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const databaseName = configService.get<string>('DB_NAME');
+        const databaseUser = configService.get<string>('DB_USER');
+        const databasePass = configService.get<string>('DB_PASS');
+        const host = configService.get<string>('HOST');
+        const databasePort = configService.get<string>('PORT_DB');
+        const uri = `mongodb://${databaseUser}:${databasePass}@${host}:${databasePort}/${databaseName}`;
+        return { uri };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const defaultPort = 3000;
+  const environmentPort = process.env.PORT_APP;
+  const port = environmentPort ? parseInt(environmentPort, 10) : defaultPort;
+  await app.listen(port);
 }
 bootstrap();
